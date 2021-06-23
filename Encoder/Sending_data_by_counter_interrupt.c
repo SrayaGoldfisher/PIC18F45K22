0ccd1a0fe67715585b0e0f35852e6109c322d3da
SrayaGoldfisher <47556043+SrayaGoldfisher@users.noreply.github.com>
2021-06-23 11:52:37 +0300

    Add description and explanation to INTCON & T0CON

--- Encoder/Sending_data_by_counter_interrupt.c
+++ Encoder/Sending_data_by_counter_interrupt.c
@@ -1,3 +1,6 @@
+//A program that detects axis rotations by an encoder and transmits the data to the PORTC via a Counter interrupt
+
+
 void interrupt() {
      if(TMR0IF_bit) {
                     LATC++;
@@ -11,8 +14,8 @@ void main() {
      ANSELA = ANSELC = 0;
      TRISA = 0xFF;
      TRISC = 0;
-     INTCON = 0b11101000;
-     T0CON = 0b11101000;
+     INTCON = 0b11101000;     //Enables Timer0, 8-bit, Transition on RA4/T0CKI pin
+     T0CON = 0b11101000;      //Increment on low-to-high, TImer0 prescaler is NOT assigned
      TMR0L = 255;
      TMR0IF_bit = 0;
      Delay_ms(50);
