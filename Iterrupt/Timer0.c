7ebf7d1c21b12b2d5ea8753b8f2a8cb4e3a70643
SrayaGoldfisher <47556043+SrayaGoldfisher@users.noreply.github.com>
2021-06-23 14:58:35 +0300

    Description, explanation and calculate
    Add a description, explanation to T0CON and calculate the prescale value

--- Iterrupt/Timer0.c
+++ Iterrupt/Timer0.c
@@ -1,3 +1,7 @@
+//Program that causes RC2 to flash half a second on "1" and half a second on "0"
+//The program is executed using the interrupt of timer 0
+//[1/32M(osc.)]*4(internal instruction)*64(prescaler)*(2^16)(TMR0) = 0.524[sec]
+
 void interrupt() {
      if (TMR0IF_bit) {
         RC2_bit =~ RC2_bit;
@@ -11,6 +15,6 @@ void main() {
      PORTC = 0;
      INTCON = 0b11100000;
      TMR0IP_bit = 1;
-     T0CON = 0b10010101;
+     T0CON = 0b10010101;      //Enables Timer0, 16-bit, CLKOUT, T0CKI on high-o-low, Prescaler is assigned, 1:64 prescale value
      TMR0IE_bit = 1;
 }
