5f2bdef121ba6be8962a9b53ef7ed88302d8c86b
SrayaGoldfisher <47556043+SrayaGoldfisher@users.noreply.github.com>
2021-06-23 12:49:05 +0300

    Add a description

--- Counters/Switching_on_and_off_every_4_clicks.c
+++ Counters/Switching_on_and_off_every_4_clicks.c
@@ -1,9 +1,14 @@
+//A program that receives input from a push-button
+//After the fourth press, an LED will light in RD3
+//The next fourth click will turn off the LED
+
+
 sbit LED at RD3_bit;
 
 void interrupt() {
      if(TMR0IF_bit) {
-                    TMR0L = 252;
-                    LED =~ LED;
+          TMR0L = 252;
+          LED =~ LED;
      }
      TMR0IF_bit = 0;
 }
