e7e0950896044c6bd995df50802b4eeb35da7be2
SrayaGoldfisher <47556043+SrayaGoldfisher@users.noreply.github.com>
2021-06-23 14:19:13 +0300

    Add a description

--- PWM/Create_a_PWM_20_80_by_compiler_functions.c
+++ PWM/Create_a_PWM_20_80_by_compiler_functions.c
@@ -1,3 +1,8 @@
+//Program that generates 80% DC PWM signal
+//(80% of cycle time on high and 20% of cycle time on low)
+//The program is executed using the PWM's built-in commands from the compiler's function library
+
+
 void main() {
 	LATC = TRISC = ANSELC = 0;
 	PWM1_Init(1953);
