26f6a8e6de38dd370b04f4e8b2feb28fdea9bee0
SrayaGoldfisher <47556043+SrayaGoldfisher@users.noreply.github.com>
2021-06-23 11:35:06 +0300

    Add a description

--- EEPROM/Displays_data_by_number_of_clicks.c
+++ EEPROM/Displays_data_by_number_of_clicks.c
@@ -1,8 +1,13 @@
+//A program that displays data stored in memory according to the number of clicks on the push button
+//One click of the button will display the first data sent to PORTD
+//Two clicks will display the second data and so on
+
+
 char tmp = 0, i = 0, j = 0;
 
 void interrupt() {
      if(RBIF_bit) {
-                  if(RB5_bit) {			//Push-button to read data from eeprom
+             if(RB5_bit) {			               //Push-button to read data from eeprom
                               j++;
                   }
                   RBIF_bit = 0;
@@ -15,21 +20,21 @@ void main() {
      PORTB = PORTC = PORTD = 0;
      TRISB = 0xFF;
      TRISC = TRISD = 0;
-     UART1_Init(9600);					//Initialize hardware UART1 and establish communication at 9600 bps
-     INTCON = 0b11001000;				//Enable Global Interrupt, Peripheral Interrupt and Port B Interrupt-On-Change
-     IOCB5_bit = 1;						//Enable Interrupt-On-Change at RB5_bit
+     UART1_Init(9600);					          //Initialize hardware UART1 and establish communication at 9600 bps
+     INTCON = 0b11001000;				          //Enable Global Interrupt, Peripheral Interrupt and Port B Interrupt-On-Change
+     IOCB5_bit = 1;						          //Enable Interrupt-On-Change at RB5_bit
 
      while(1) {
               while(i < 6) {						//Write and show data to 0-7 first address in the eeprom
                       Delay_ms(1000);
                       tmp = UART1_Read();			//Save the data to variable tmp
-                      EEPROM_Write(0x00+i, tmp);	//Write the data to the eeprom
+                      EEPROM_Write(0x00+i, tmp);	     //Write the data to the eeprom
                       UART1_Write(i);				//Show which adress the data write
                       i++;
                       Delay_ms(250);
               }
               i = 0;
-              if(j > 0) {							//Enter if was click on the push-button by the interrupt RBIF
+         if(j > 0) {							//Enter if was click on the push-button by the interrupt RBIF
                    LATD = EEPROM_Read(0x00+j);		//Show the data from the eeprom
                    j = 0;
                    Delay_ms(1000);
