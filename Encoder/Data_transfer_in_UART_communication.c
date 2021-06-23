3a51b23dfd80dd8b1705f77a8161ae453c9732fc
SrayaGoldfisher <47556043+SrayaGoldfisher@users.noreply.github.com>
2021-06-23 12:39:49 +0300

    Reload of the changes

--- Encoder/Data_transfer_in_UART_communication.c
+++ Encoder/Data_transfer_in_UART_communication.c
@@ -1,11 +1,10 @@
 //A program that transmits the data to the terminal via serial communication - UART
 
-int tmp;
 
 void interrupt() {
      if(TMR0IF_bit) {
-                    LATD++;
-                    TMR0L = 255;
+          LATD++;
+          TMR0L = 255;
      }
      TMR0IF_bit = 0;
 }
@@ -16,7 +15,7 @@ void main() {
      TRISA = TRISB = 0xF7;
      TRISC = TRISD = 0;
      INTCON = 0b11101000;     //Enables Timer0, 8-bit, Transition on RA4/T0CKI pin
-     T0CON = 0b10100000;      
+     T0CON = 0b11101000;      //Increment on low-to-high, TImer0 prescaler is NOT assigned     
      TMR0L = 255;
      TMR0IF_bit = 0;
      Delay_ms(50);
@@ -24,8 +23,8 @@ void main() {
      Delay_ms(100);
      UART1_Write_Text("start");
      while(1) {
-          tmp = LATC >> 2;
-          UART2_Write(tmp);
+          LATA3_bit = 1; 
+          UART1_Write(LATD); 
           Delay_ms(1000);
      }
 }
