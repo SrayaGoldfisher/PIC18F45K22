31a621687304d2ff182ab8d586e8ffa1f2c08ab3
SrayaGoldfisher <47556043+SrayaGoldfisher@users.noreply.github.com>
2021-06-23 12:52:59 +0300

    Add a description

--- Counters/LED_lighting_by_counter.c
+++ Counters/LED_lighting_by_counter.c
@@ -1,10 +1,15 @@
+//Using the COUNTER module I wrote a program that receives input from a push-button
+//When the system detects 2 clicks it performs a countdown on the PORTC from 10 to 0
+//When the count is complete the system will turn on LED at RD0
+
+
 sbit RED_LED at RD0_bit;
 int two_press = 0;
 
 void interrupt() {
      if(TMR0IF_bit) {
-                    TMR0L = 254;
-                    two_press = 1;
+          TMR0L = 254;
+          two_press = 1;
      }
      TMR0IF_bit = 0;
 }
@@ -20,16 +25,16 @@ void main() {
      TMR0L = 254;
      Delay_ms(50);
      while(1) {
-              while((LATC > 0) && (two_press)) {
-                          Delay_ms(1000);
-                          LATC--;
-              }
-              if(!LATC) {
-                        RED_LED = 1;
-                        Delay_ms(1000);
-                        RED_LED = 0;
-                        PORTC = 10;
-                        two_press = 0;
-              }
+          while((LATC > 0) && (two_press)) {
+               Delay_ms(1000);
+               LATC--;
+          }
+          if(!LATC) {
+               RED_LED = 1;
+               Delay_ms(1000);
+               RED_LED = 0;
+               PORTC = 10;
+               two_press = 0;
+          }
      }
 }
