c95e2d1b5397fd59ceb817adea8a906efda6d4db
SrayaGoldfisher <47556043+SrayaGoldfisher@users.noreply.github.com>
2021-06-23 11:46:49 +0300

    Add program description and explanation to INTCON

--- Encoder/Sending_data_by_push_button_interrupt.c
+++ Encoder/Sending_data_by_push_button_interrupt.c
@@ -1,3 +1,6 @@
+//A program that detects axis rotations by an encoder and transmits the data to PORTC via a button interrupt
+
+
 void interrupt() {
      if (INT0IF_bit) {
         if(RB0_bit){
@@ -13,7 +16,7 @@ void main() {
      PORTC = 0;
      TRISB = 0xFF;
      PORTB = 0;
-     INTCON = 0b11010000;
+     INTCON = 0b11010000;     //interrupte enable, peie enable, INT0 External Interrupt Enable, clear int0 flag
      INT0IF_bit = 0;
      Delay_ms(100);
      while(1);
