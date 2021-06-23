320a4143ccaec4931e7c447a5e880bf7ddee21d3
SrayaGoldfisher <47556043+SrayaGoldfisher@users.noreply.github.com>
2021-06-23 11:56:55 +0300

    Add description and explanation to INTCON & T0CON

--- Encoder/LED_lighting_according_to_the_direction_of_the_motor.c
+++ Encoder/LED_lighting_according_to_the_direction_of_the_motor.c
@@ -1,3 +1,7 @@
+//A program that turns on a green LED when the engine is rotating in one direction 
+//and a red LED when the engine is rotating in the opposite direction
+
+
 sbit RED_LED at RD0_bit;
 sbit GREEN_LED at RD1_bit;
 
@@ -22,8 +26,8 @@ void main() {
      ANSELA = ANSELB = ANSELC = ANSELD = 0;
      TRISA = TRISB = 0xFF;
      TRISC = TRISD = 0;
-     INTCON = 0b11101000;
-     T0CON = 0b11101000;
+     INTCON = 0b11101000;     //Enables Timer0, 8-bit, Transition on RA4/T0CKI pin
+     T0CON = 0b11101000;      //Increment on low-to-high, TImer0 prescaler is NOT assigned
      TMR0L = 255;
      TMR0IF_bit = 0;
      Delay_ms(50);
