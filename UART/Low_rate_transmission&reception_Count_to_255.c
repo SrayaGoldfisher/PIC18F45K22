6074cab972de16fac7ba683069b73e0db84edb85
SrayaGoldfisher <47556043+SrayaGoldfisher@users.noreply.github.com>
2021-06-24 10:41:58 +0300

    Add a description

--- UART/Low_rate_transmission&reception_Count_to_255.c
+++ UART/Low_rate_transmission&reception_Count_to_255.c
@@ -1,3 +1,10 @@
+//A program that creates transmission and reception, at a low bit rate of 1200kbps
+//Communication using the UART protocol of the controller
+//The program will count binary from 1 to 255 in PORTB in an infinite loop
+//The broadcast house will be entered into PORTB
+//The transmission and reception will be performed in one controller, ie we will connect the transmission pin Tx to the reception pin Rx
+
+
 void main() {
      int i;
      PORTB = 0;
@@ -9,6 +16,7 @@ void main() {
      TXSTA = 0b00100010;
      RCSTA = 0b10010000;
      TXREG = 0;
+     
      do {
         TXREG++;
         while (!TRMT_bit);
